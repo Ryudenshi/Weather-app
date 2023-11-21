5b0c7012c993fb7ad5f061b4398bd662adce6cf1
Ryudenshi <skarydog17@gmail.com>
2023-11-21 10:11:36 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Weather app_Letz Yevhen_IPZm-11"
+include(":app")
+ 
