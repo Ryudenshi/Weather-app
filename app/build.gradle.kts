cb813cfcc73fa7aef13abe9363560968e38d17b7
Ryudenshi <skarydog17@gmail.com>
2023-11-21 12:38:20 +0200

    main design finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.weatherapp_letzyevhen_ipzm_11"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.weatherapp_letzyevhen_ipzm_11"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
